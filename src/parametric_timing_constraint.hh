a37195130638dc75a5a58107936f2548b75e84e4
Masaki Waga <masakiwaga@gmail.com>
2025-05-24 20:48:23 +0900

    Added shift to ParametricTimingConstraint

--- src/parametric_timing_constraint.hh
+++ src/parametric_timing_constraint.hh
@@ -14,10 +14,22 @@
 using ParametricTimingValuation = Parma_Polyhedra_Library::NNC_Polyhedron;
 using ParametricTimingConstraint = Parma_Polyhedra_Library::NNC_Polyhedron;
 
-static inline
-bool eval(ParametricTimingValuation &cval, const ParametricTimingConstraint &guard) {
-  cval.intersection_assign(guard);
-  return !cval.is_empty();
+static bool eval(ParametricTimingValuation &cval, const ParametricTimingConstraint &guard) {
+    cval.intersection_assign(guard);
+    return !cval.is_empty();
+}
+
+/**
+ * @brief Shift the clock variable id of the guard by a given width.
+ *
+ * @param guard the guard to shift
+ * @param width the width to shift the clock variable id
+ * @return a new ParametricTimingConstraint with the clock variable shifted
+ */
+static ParametricTimingConstraint shift(const ParametricTimingConstraint &guard, const std::size_t width) {
+    auto result = Parma_Polyhedra_Library::NNC_Polyhedron(width, Parma_Polyhedra_Library::UNIVERSE);
+    result.concatenate_assign(guard);
+    return result;
 }
 
 #endif //DATAMONITOR_PARAMETRIC_TIMING_CONSTRAINT_HH
