3a9633fc52148003b0368ef0e191badae0f0c111
taiseiKMC <taisei@kmc.gr.jp>
2025-12-23 03:27:00 +0900

    Try to implement unobservable transition in DataParametricMonitor

--- src/data_parametric_monitor.hh
+++ src/data_parametric_monitor.hh
@@ -27,8 +27,8 @@ struct DataParametricMonitorResult {
 class DataParametricMonitor : public SingleSubject<DataParametricMonitorResult>,
                               public Observer<TimedWordEvent<PPLRational>> {
 public:
+  static const constexpr std::size_t unobservableActionID = 127;
   explicit DataParametricMonitor(const DataParametricTA &automaton) : automaton(automaton) {
-    absTime = 0;
     configurations.clear();
     // configurations.reserve(automaton.initialStates.size());
     std::vector<double> initCVal(automaton.clockVariableSize);
@@ -37,7 +37,7 @@ public:
     // by default, initNEnv is the universe of dimension automaton.numberVariableSize
     Symbolic::NumberValuation initNEnv(automaton.numberVariableSize);
     for (const auto &initialState: automaton.initialStates) {
-      configurations.insert({initialState, initCVal, initSEnv, initNEnv});
+      configurations.insert({initialState, initCVal, initSEnv, initNEnv, 0});
     }
   }
 
@@ -49,10 +49,68 @@ public:
     const std::vector<PPLRational> &numbers = event.numbers;
     const double timestamp = event.timestamp;
     boost::unordered_set<Configuration> nextConfigurations;
+    boost::unordered_set<Configuration> currentConfigurations;
+    for (Configuration conf: configurations) {
+      // add a new dimension for time elapse.
+      currentConfigurations.insert(std::move(conf));
+    }
+    while (!currentConfigurations.empty()) {
+      nextConfigurations.clear();
+      for (const Configuration &conf: currentConfigurations) {
+        // この unobservableActionID が epsilon 遷移に対応している
+        auto transitionIt = std::get<0>(conf)->next.find(unobservableActionID);
+        if (transitionIt == std::get<0>(conf)->next.end()) {
+          continue;
+        }
+        // make the current env
+        const auto clockValuation = std::get<1>(conf);
+        const auto stringEnv = std::get<2>(conf);
+        const auto numberEnv = std::get<3>(conf);
+        auto absTime = std::get<4>(conf);
+        for (const auto &transition: transitionIt->second) {
+          // evaluate the guards
+          auto nextCVal = clockValuation;
+          auto nextSEnv = stringEnv;
+          auto nextNEnv = numberEnv;
+          auto extendedGuard = transition.guard;
+          
+          // clock の guard に, 不等式による制約がないことを仮定する
+          auto df = diff(nextCVal, extendedGuard);
+          // FIXME: when(= 1), when(=2) があると, d + 1 + 2 されて absTime になってしまう
+          if(!df) {
+            throw std::runtime_error("DataParametricMonitor: unsupported guard with inequality constraints on unobservable transition");
+          }
+          for (Timestamp &d: nextCVal) {
+            d += df.value();
+          }
+          absTime += df.value();
+
+          if (eval(nextCVal, extendedGuard) &&
+              eval(transition.stringConstraints, nextSEnv, transition.numConstraints, nextNEnv)) {
+            for (const VariableID resetVar: transition.resetVars) {
+              nextCVal[resetVar] = 0;
+            }
+            transition.update.execute(nextSEnv, nextNEnv);
+            nextConfigurations.insert({transition.target.lock(), nextCVal, nextSEnv, nextNEnv, absTime});
+            if (transition.target.lock()->isMatch) {
+              this->notifyObservers({index, absTime, nextNEnv, nextSEnv});
+            }
+            configurations.insert({transition.target.lock(), nextCVal, nextSEnv, nextNEnv, absTime});
+          }
+        }
+      }
+
+      std::swap(currentConfigurations, nextConfigurations);
+    }
+    nextConfigurations.clear();
 
     for (const Configuration &conf: configurations) {
       // make the current env
       auto clockValuation = std::get<1>(conf); //.clockValuation;
+      const auto absTime = std::get<4>(conf);
+      if(timestamp < absTime) {
+        continue;
+      }
       for (double &d: clockValuation) {
         d += timestamp - absTime;
       }
@@ -83,14 +141,13 @@ public:
           transition.update.execute(nextSEnv, nextNEnv);
           nextSEnv.resize(automaton.stringVariableSize);
           nextNEnv.remove_higher_space_dimensions(automaton.numberVariableSize);
-          nextConfigurations.insert({transition.target.lock(), std::move(nextCVal), nextSEnv, nextNEnv});
+          nextConfigurations.insert({transition.target.lock(), std::move(nextCVal), nextSEnv, nextNEnv, timestamp});
           if (transition.target.lock()->isMatch) {
             notifyObservers({index, timestamp, nextNEnv, nextSEnv});
           }
         }
       }
     }
-    absTime = timestamp;
     index++;
     configurations = std::move(nextConfigurations);
   }
@@ -98,7 +155,7 @@ public:
 private:
   const DataParametricTA automaton;
   using Configuration = std::tuple<std::shared_ptr<DataParametricTAState>, std::vector<double>,
-                                   Symbolic::StringValuation, Symbolic::NumberValuation>;
+                                   Symbolic::StringValuation, Symbolic::NumberValuation, double>;
   // Symbolic::NumberValuation>;
   /*  struct Configuration {
       std::shared_ptr<DataParametricTAState> state;
@@ -107,6 +164,5 @@ private:
       Symbolic::NumberValuation numberEnv;
     };*/
   boost::unordered_set<Configuration> configurations;
-  double absTime;
   std::size_t index = 0;
 };
