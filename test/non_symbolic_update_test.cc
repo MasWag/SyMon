99d1448bc0f9a9a0159e81c8c658ea9e542e3628
taiseiKMC <taisei@kmc.gr.jp>
2026-01-03 02:48:05 +0900

    Add tests for symbolic/non-symbolic string updates

--- test/non_symbolic_update_test.cc
+++ test/non_symbolic_update_test.cc
@@ -52,5 +52,68 @@ BOOST_AUTO_TEST_SUITE(NonSymbolicUpdateTests)
       //The evaluation of x0 is updated to 2 before evaluating x2
       BOOST_CHECK_EQUAL(numEnv[2].value(), 12);
     }
+
+    BOOST_AUTO_TEST_CASE(string_execute) {
+      std::vector<std::pair<VariableID, NonSymbolic::StringAtom>> stringUpdate = {
+        {0, NonSymbolic::StringAtom{VariableID{1}}},
+        {1, NonSymbolic::StringAtom{std::string{"Charlie"}}},
+
+        {0, NonSymbolic::StringAtom{VariableID{1}}}
+      };
+      NonSymbolic::StringValuation stringEnv = {std::make_optional("Alice"), std::make_optional("Bob")};
+      std::vector<std::pair<VariableID, Expr>> numUpdate;
+      NonSymbolic::NumberValuation<int> numEnv;
+
+      NonSymbolic::Update<int> update = {stringUpdate, numUpdate};
+      update.execute(stringEnv, numEnv);
+      BOOST_CHECK_EQUAL(stringEnv[0].value(), "Charlie");
+      BOOST_CHECK_EQUAL(stringEnv[1].value(), "Charlie");
+    }
+
+    BOOST_AUTO_TEST_CASE(stringExecute2) {
+      NonSymbolic::NumberValuation<int> numEnv;
+      std::vector<NonSymbolic::NumberConstraint<int>> numberConstraints;
+      std::vector<std::pair<VariableID, NonSymbolic::NumberExpression<int>>> numUpdate;
+
+      NonSymbolic::StringValuation stringEnv = {std::nullopt, std::nullopt};
+      std::vector<NonSymbolic::StringConstraint> stringConstraints1 = {
+        NonSymbolic::SCMaker(0) != "Alice",
+        NonSymbolic::SCMaker(0) != "Bob",
+        NonSymbolic::SCMaker(0) != "Charlie",
+        NonSymbolic::SCMaker(1) != "Alice",
+        NonSymbolic::SCMaker(1) != "Bob"
+      };
+      BOOST_TEST(NonSymbolic::eval(stringConstraints1, stringEnv, numberConstraints, numEnv));
+    
+      std::vector<std::pair<VariableID, NonSymbolic::StringAtom>> stringUpdate = {
+        {1, NonSymbolic::StringAtom{VariableID{0}}},
+        {0, NonSymbolic::StringAtom{std::string{"Charlie"}}},
+      };
+      NonSymbolic::Update<int> update = {stringUpdate, numUpdate};
+      update.execute(stringEnv, numEnv);
+      // After the update, stringEnv[0] is "Charlie", and stringEnv[1] is disabled for "Alice", "Bob", "Charlie"
+      BOOST_CHECK_EQUAL(*stringEnv[0], "Charlie");
+      //BOOST_CHECK_EQUAL(std::get<std::vector<std::string>>(stringEnv[1]).size(), 3);
+
+      //std::vector<NonSymbolic::StringConstraint> stringConstraints2 = {NonSymbolic::SCMaker(1) == "Charlie"};
+      //BOOST_TEST(!NonSymbolic::eval(stringConstraints2, stringEnv, numberConstraints, numEnv));
+    
+      std::vector<NonSymbolic::StringConstraint> stringConstraints3 = {
+        NonSymbolic::SCMaker(0) != "Alice",
+        NonSymbolic::SCMaker(0) != "Bob",
+        NonSymbolic::SCMaker(0) == "Charlie",
+        NonSymbolic::SCMaker(0) != "David"
+      };
+      BOOST_TEST(NonSymbolic::eval(stringConstraints3, stringEnv, numberConstraints, numEnv));
+      
+      std::vector<NonSymbolic::StringConstraint> stringConstraints4 = {NonSymbolic::SCMaker(1) == "David"};
+      // stringEnv[1] is assumed to be "David"
+      BOOST_TEST(NonSymbolic::eval(stringConstraints4, stringEnv, numberConstraints, numEnv));
+
+      //std::vector<NonSymbolic::StringConstraint> stringConstraints5 = {NonSymbolic::SCMaker(1) == "Eve"};
+      // stringEnv[1] is assumed to be "David" above, so it cannot be "Eve"
+      //BOOST_TEST(!NonSymbolic::eval(stringConstraints5, stringEnv, numberConstraints, numEnv));
+    
+    }
   BOOST_AUTO_TEST_SUITE_END() // EvalTests
 BOOST_AUTO_TEST_SUITE_END() // NonSymbolicUpdateTests
--- test/symbolic_update_test.cc
+++ test/symbolic_update_test.cc
@@ -149,6 +149,67 @@ BOOST_AUTO_TEST_SUITE(SymbolicDataConstraintsTest)
       BOOST_TEST((numEnv == expectedNumEnv));
     }
 
+    BOOST_AUTO_TEST_CASE(stringExecute) {
+      std::vector<std::pair<VariableID, Symbolic::StringAtom>> stringUpdate = {
+        {0, Symbolic::StringAtom{VariableID{1}}},
+        {1, Symbolic::StringAtom{std::string{"Charlie"}}},
+
+        {0, Symbolic::StringAtom{VariableID{1}}}
+      };
+      Symbolic::StringValuation stringEnv = {std::string("Alice"), std::string("Bob")};
+      std::vector<std::pair<VariableID, Symbolic::NumberExpression>> numUpdate;
+      Symbolic::NumberValuation numEnv;
+      Symbolic::Update update = {stringUpdate, numUpdate};
+      update.execute(stringEnv, numEnv);
+      BOOST_CHECK_EQUAL(std::get<std::string>(stringEnv[0]), "Charlie");
+      BOOST_CHECK_EQUAL(std::get<std::string>(stringEnv[1]), "Charlie");
+    }
+
+    BOOST_AUTO_TEST_CASE(stringExecute2) {
+      Parma_Polyhedra_Library::NNC_Polyhedron numEnv;
+      std::vector<Symbolic::NumberConstraint> numberConstraints;
+      std::vector<std::pair<VariableID, Symbolic::NumberExpression>> numUpdate;
+
+      Symbolic::StringValuation stringEnv = {std::vector<std::string>{}, std::vector<std::string>{}};
+      std::vector<Symbolic::StringConstraint> stringConstraints1 = {
+        Symbolic::SCMaker(0) != "Alice",
+        Symbolic::SCMaker(0) != "Bob",
+        Symbolic::SCMaker(0) != "Charlie",
+        Symbolic::SCMaker(1) != "Alice",
+        Symbolic::SCMaker(1) != "Bob"
+      };
+      BOOST_TEST(Symbolic::eval(stringConstraints1, stringEnv, numberConstraints, numEnv));
+    
+      std::vector<std::pair<VariableID, Symbolic::StringAtom>> stringUpdate = {
+        {1, Symbolic::StringAtom{VariableID{0}}},
+        {0, Symbolic::StringAtom{std::string{"Charlie"}}},
+      };
+      Symbolic::Update update = {stringUpdate, numUpdate};
+      update.execute(stringEnv, numEnv);
+      // After the update, stringEnv[0] is "Charlie", and stringEnv[1] is disabled for "Alice", "Bob", "Charlie"
+      BOOST_CHECK_EQUAL(std::get<std::string>(stringEnv[0]), "Charlie");
+      BOOST_CHECK_EQUAL(std::get<std::vector<std::string>>(stringEnv[1]).size(), 3);
+
+      std::vector<Symbolic::StringConstraint> stringConstraints2 = {Symbolic::SCMaker(1) == "Charlie"};
+      BOOST_TEST(!Symbolic::eval(stringConstraints2, stringEnv, numberConstraints, numEnv));
+    
+      std::vector<Symbolic::StringConstraint> stringConstraints3 = {
+        Symbolic::SCMaker(0) != "Alice",
+        Symbolic::SCMaker(0) != "Bob",
+        Symbolic::SCMaker(0) == "Charlie",
+        Symbolic::SCMaker(0) != "David"
+      };
+      BOOST_TEST(Symbolic::eval(stringConstraints3, stringEnv, numberConstraints, numEnv));
+
+      std::vector<Symbolic::StringConstraint> stringConstraints4 = {Symbolic::SCMaker(1) == "David"};
+      // stringEnv[1] is assumed to be "David"
+      BOOST_TEST(Symbolic::eval(stringConstraints4, stringEnv, numberConstraints, numEnv));
+
+      std::vector<Symbolic::StringConstraint> stringConstraints5 = {Symbolic::SCMaker(1) == "Eve"};
+      // stringEnv[1] is assumed to be "David" above, so it cannot be "Eve"
+      BOOST_TEST(!Symbolic::eval(stringConstraints5, stringEnv, numberConstraints, numEnv));
+    
+    }
   BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE_END()
