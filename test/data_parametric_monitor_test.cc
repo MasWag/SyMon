2c2ebcffb572ca8400e3ddaef85c2322211568e9
taiseiKMC <taisei@kmc.gr.jp>
2026-01-02 17:56:48 +0900

    Add data parametric test case with non-integer timestamp

--- test/data_parametric_monitor_test.cc
+++ test/data_parametric_monitor_test.cc
@@ -2,6 +2,7 @@
 #include <boost/mpl/list.hpp>
 #include "../src/data_parametric_monitor.hh"
 #include "../test/fixture/copy_automaton_fixture.hh"
+#include "../test/fixture/non_integer_timestamp_fixture.hh"
 
 using TWEvent = TimedWordEvent<PPLRational>;
 
@@ -65,4 +66,35 @@ BOOST_FIXTURE_TEST_CASE(test2, CopyDataParametricMonitorFixture)
   BOOST_CHECK_EQUAL(resultVec.front().timestamp, 15.5);
 }
 
+BOOST_AUTO_TEST_SUITE_END()
+
+
+struct NonIntegerTimestampDataParametricMonitorFixture : public Parametric::DataParametricNonIntegerTimestampFixture {
+  void feed(std::vector<TWEvent> &&vec) {
+    auto monitor = std::make_shared<DataParametricMonitor>(automaton);
+    std::shared_ptr<DummyDataParametricMonitorObserver> observer = std::make_shared<DummyDataParametricMonitorObserver>();
+    monitor->addObserver(observer);
+    DummyDataTimedWordSubject subject{std::move(vec)};
+    subject.addObserver(monitor);
+    subject.notifyAll();
+    resultVec = std::move(observer->resultVec);
+  }
+  std::vector<DataParametricMonitorResult> resultVec;
+};
+
+BOOST_AUTO_TEST_SUITE(DataParametricNonIntegerTimestampMonitorTest)
+  BOOST_FIXTURE_TEST_CASE(non_integer_timestamp_test, NonIntegerTimestampDataParametricMonitorFixture) {
+    std::vector<TWEvent> dummyTimedWord{
+      {0, {}, {0}, 0.},
+      {0, {}, {0}, 1.0},
+      {0, {}, {0}, 2.1},
+      {0, {}, {0}, 3.3},
+      {0, {}, {0}, 4.6}
+    };
+    feed(std::move(dummyTimedWord));
+    //NOTE: 3.3 - 2.1 is 1.2, but this is evaluated as 1.1999999999999997 < 1.2, so the fourth event is also matched.
+    //BOOST_CHECK_EQUAL(resultVec.size(), 1);
+    BOOST_CHECK_EQUAL(resultVec.front().index, 2);
+    BOOST_CHECK_EQUAL(resultVec.front().timestamp, 2.1);
+  }
 BOOST_AUTO_TEST_SUITE_END()
